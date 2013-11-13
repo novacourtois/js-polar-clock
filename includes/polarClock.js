c82730f94971b9780781f40e7d936a18457e798a
Yves Courtois <Courtois1337@gmail.com>
2013-11-12 18:48:17 -0600

    added index, changed color

--- includes/polarClock.js
+++ includes/polarClock.js
@@ -9,6 +9,13 @@ if(canvas.width > canvas.height)
 if(canvas.height > canvas.width)
     canvas.height = canvas.width;
 
+var arcColor = ['rgba(0,255,0,1)',
+               'rgba(253,105,0,1)',
+               'rgba(35,232,183,1)',
+               'rgba(230,11,66,1)',
+               'rgba(252,219,0,1)',                
+               'rgba(255,255,255,1)'];
+
 function clock(){
     var now = new Date();
     ctx.save();
@@ -32,13 +39,36 @@ function clock(){
     if (hrpm > 12)
         hrpm -= 12;
     
-    ctx.fillStyle = "fff";
+    ctx.fillStyle = '#ccc';
     ctx.font = "42px Georgia";
     ctx.textAlign = "center";
     ctx.textBaseline = "top";
     ctx.fillText(Math.floor(hrpm)+" : "+Math.floor(min)+" : "+Math.floor(sec), canvas.width/2, canvas.height/2 + 24);
    
     ctx.fillText("JavaScript Polar Clock",canvas.width/2, canvas.height/2 - 24);
+
+
+    ctx.font = "12px Georgia";
+    ctx.textAlign = "left";
+
+    ctx.fillStyle = arcColor[5];
+    ctx.fillText("Seconds",0, 0);
+
+    ctx.fillStyle = arcColor[4];
+    ctx.fillText("Minutes",0, 14);
+
+    ctx.fillStyle = arcColor[3];
+    ctx.fillText("Hour",0, 28);
+
+    ctx.fillStyle = arcColor[2];
+    ctx.fillText("Day of week",0, 42);
+
+    ctx.fillStyle = arcColor[1];
+    ctx.fillText("Day of month",0, 56);
+
+    ctx.fillStyle = arcColor[0];
+    ctx.fillText("Month",0, 70);
+
     ctx.rotate(-Math.PI/2);
     
     var secPer = sec/60;
@@ -62,7 +92,7 @@ function clock(){
         dayPer = day/30;
     }
     
-    writeTime(ctx,canvas.width/3 + 0,dayPer,0);
+    writeTime(ctx,canvas.width/3 + 0,monthPer,0);
     writeTime(ctx,canvas.width/3 + 15,dayPer,1);
     writeTime(ctx,canvas.width/3 + 30,dowPer,2);
     writeTime(ctx,canvas.width/3 + 45,hrPer,3);
@@ -73,12 +103,6 @@ function clock(){
 }
     
 function writeTime(ctx,radius,per, index){
-    var arcColor = ['rgba(0,255,0,1)',
-                    'rgba(253,105,0,1)',
-                    'rgba(35,232,183,1)',
-                    'rgba(252,219,0,1)',
-                    'rgba(224,27,178,1)',
-                    'rgba(255,255,255,1)'];
     ctx.save();
     ctx.strokeStyle = arcColor[index]; 
     ctx.beginPath();
